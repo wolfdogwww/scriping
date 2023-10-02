16fa9ed622632d517886f26b0372ca7abf5f2722
worlfdog <abc1276@kimo.com>
2023-10-02 19:25:51 +0800

    try

---
+++ test01.py
@@ -0,0 +1,15 @@
+def addnum (x,y):
+    return x+y
+
+def subnum (x,y):
+    return x-y
+
+def mulnum (x,y):
+    return x*y
+
+def divnum (x,y):
+    return x/y
+
+first = int(input("pls input first number："))
+second = int(input("pls input second number："))
+print(addnum(first,second))
